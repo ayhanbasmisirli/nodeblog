907bd552dcdaa28b09926ee3d6b7cbedbdb1555d
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2015-10-15 11:42:04 +0300

    blog post added front page style added

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+//making local variable as moment
+app.locals.moment = require('moment');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
